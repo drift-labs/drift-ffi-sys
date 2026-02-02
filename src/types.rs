2901f860b76fef6bc9528bcc10ee69e72e428dd7
jordy25519 <beauchjord@gmail.com>
2026-02-02 15:16:11 +0800

    remove align(16) from some types

--- src/types.rs
+++ src/types.rs
@@ -104,11 +104,11 @@ pub struct MarginCalculation {
 #[repr(C)]
 #[derive(Copy, Clone, Debug, Default)]
 pub struct IsolatedMarginCalculation {
-    pub market_index: u16,
     pub margin_requirement: compat::u128,
     pub total_collateral: compat::i128,
     pub total_collateral_buffer: compat::i128,
     pub margin_requirement_plus_buffer: compat::u128,
+    pub market_index: u16,
 }
 
 impl MarginCalculation {
